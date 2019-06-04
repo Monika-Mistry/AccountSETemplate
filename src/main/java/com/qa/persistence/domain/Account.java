14a51153061661bb3658ccc2fc1d530cff0e874e
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-04 16:41:37 +0100

    Account changes

--- src/main/java/com/qa/persistence/domain/Account.java
+++ src/main/java/com/qa/persistence/domain/Account.java
@@ -2,14 +2,18 @@ package com.qa.persistence.domain;
 
 public class Account {
 
+	private int id;
 	private int accountNumber;
 	private String firstName;
 	private String lastName;
+	private static int counter = 1;
 
 	public Account(int accountNumber, String firstName, String lastName) {
+		this.setId(counter);
 		this.accountNumber = accountNumber;
 		this.firstName = firstName;
 		this.lastName = lastName;
+		counter++;
 	}
 
 	public Account() {
@@ -38,6 +42,14 @@ public class Account {
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 
 }
