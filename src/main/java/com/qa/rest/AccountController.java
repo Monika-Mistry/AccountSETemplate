0dae222d0f7f283bc407dd4a21ab01fd48de3231
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-06 16:23:04 +0100

    @Produces annotations added

--- src/main/java/com/qa/rest/AccountController.java
+++ src/main/java/com/qa/rest/AccountController.java
@@ -7,6 +7,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 import com.qa.business.service.AccountService;
 
@@ -18,31 +19,36 @@ public class AccountController {
 
 	@POST
 	@Path("/addAccount")
+	@Produces({ "application/json" })
 	public String addAccount(String account) {
 		return service.addAccount(account);
 	}
 
 	@GET
 	@Path("/getAllAccounts")
+	@Produces({ "application/json" })
 	public String getAllAccounts() {
 		return service.getAllAccounts();
 	}
 
 	@GET
 	@Path("/getAnAccount/{accountNumber}")
+	@Produces({ "application/json" })
 	public String getAnAccount(@PathParam("accountNumber") int accountNumber) {
 		return service.getAnAccount(accountNumber);
 	}
 
 	@PUT
 	@Path("/updateAccount")
+	@Produces({ "application/json" })
 	public String updateAccount(int accountNumber, String account) {
 		return service.updateAccount(accountNumber, account);
 	}
 
 	@DELETE
-	@Path("/deleteAccount")
-	public String deleteAccount(int accountNumber) {
+	@Path("/deleteAccount/{accountNumber}")
+	@Produces({ "application/json" })
+	public String deleteAccount(@PathParam("accountNumber") int accountNumber) {
 		return service.deleteAccount(accountNumber);
 	}
 }
