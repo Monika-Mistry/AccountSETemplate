018aadb5b66dcf43e812a0d17cb34e97a8febb21
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-06 15:14:15 +0100

    setting up accountcontroller

--- src/main/java/com/qa/rest/AccountController.java
+++ src/main/java/com/qa/rest/AccountController.java
@@ -1,5 +1,33 @@
 package com.qa.rest;
 
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+
+import com.qa.business.service.AccountService;
+
+@Path("/account")
 public class AccountController {
 
+	@Inject
+	private AccountService service;
+
+	public String addAccount(String account) {
+
+	}
+
+	public String getAllAccounts() {
+
+	}
+
+	public String getAnAccount(int accountNumber) {
+
+	}
+
+	public String updateAccount(int accountNumber, String account) {
+
+	}
+
+	public String deleteAccount(int accountNumber) {
+
+	}
 }
