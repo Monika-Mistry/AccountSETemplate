83f21de34572289464dd8e3ca9e8d0bf38ce2e8f
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-06 15:17:54 +0100

    adding methods for accountcontroller

--- src/main/java/com/qa/rest/AccountController.java
+++ src/main/java/com/qa/rest/AccountController.java
@@ -1,7 +1,12 @@
 package com.qa.rest;
 
 import javax.inject.Inject;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 import com.qa.business.service.AccountService;
 
@@ -11,23 +16,33 @@ public class AccountController {
 	@Inject
 	private AccountService service;
 
+	@POST
+	@Path("/addAccount")
 	public String addAccount(String account) {
-
+		return service.addAccount(account);
 	}
 
+	@GET
+	@Path("/getAllAccounts")
 	public String getAllAccounts() {
-
+		return service.getAllAccounts();
 	}
 
-	public String getAnAccount(int accountNumber) {
-
+	@GET
+	@Path("/getAnAccount/{accountNumber}")
+	public String getAnAccount(@PathParam("accountNumber") int accountNumber) {
+		return service.getAnAccount(accountNumber);
 	}
 
+	@PUT
+	@Path("/updateAccount")
 	public String updateAccount(int accountNumber, String account) {
-
+		return service.updateAccount(accountNumber, account);
 	}
 
+	@DELETE
+	@Path("/deleteAccount")
 	public String deleteAccount(int accountNumber) {
-
+		return service.deleteAccount(accountNumber);
 	}
 }
